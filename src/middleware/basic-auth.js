215de52cb5971f8ddb8d135edb7178f4afd85ba8
Martin Balke <Mrtnbalke@gmail.com>
2019-11-05 11:24:23 -0800

    race

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -44,16 +44,19 @@ module.exports = async (req, res, next) => {
   let credentials = basicDecode(encodedData);
 
   // now that we have the credentials, let's authenticate
-  let user = await users.authBasic(credentials);
-  
+  let user = await users.authBasic(credentials)
+  .then( () => {
+    if (user) {
+      req.user = user
+      next();
+    } else next('User not found'); 
+  } 
   // We store the authenticated user back into the request for
   // other route endpoints to use if the user is valid. If the
   // user is not valid, we throw the next error middleware in the
   // chain
-  user.then( (user) => {
-    if (user) {
-      req.user = user;
-      next();
-    } else next('User not found');
-  });
+  // if (user) {
+  //   req.user = user
+  //   next();
+  // } else next('User not found');
 };
