24f3cdffe2ad7334f61b9e7dac773106a8ca4221
Martin Balke <Mrtnbalke@gmail.com>
2019-11-05 10:47:20 -0800

    paths working

--- src/middleware/oauth/github-mw.js
+++ src/middleware/oauth/github-mw.js
@@ -1,5 +1,31 @@
 const superagent = require('superagent');
 
-let getUserData = async request => {};
+let getUserData = async request => {
+  let authCode = request.query.code;
+
+  // TODO: Comment
+  let githubRes = await superagent
+    .post(process.env.GITHUB_TOKEN_SERVICE)
+    .type('form')
+    .send({
+      code: authCode,
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      state: 'haha',
+      redirect_uri: `${process.env.HOME_URL}/github-oauth`,
+    });
+
+  // TODO: Comment
+  let access_token = githubRes.body.access_token;
+
+  // TODO: Comment
+  githubRes = await superagent
+    .get(process.env.GITHUB_API)
+    .set('Authorization', `Bearer ${access_token}`);
+
+  // TODO: Comment
+  let userData = githubRes.body;
+  return userData;
+};
 
 module.exports = getUserData;
--- src/routes/oauth/github.js
+++ src/routes/oauth/github.js
@@ -5,8 +5,34 @@ const express = require('express');
 const githubMW = require('../../middleware/oauth/github-mw.js');
 const router = express.Router();
 
-router.get('/github', (req, res, next) => {});
+router.get('/github', (req, res, next) => {
+  let githubOAuthURL = process.env.GITHUB_AUTH_SERVICE;
+  
+  let options = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    redirect_uri: process.env.HOME_URL + '/github-oauth',
+    scope: 'user',
+    state: 'haha',
+  };
 
-router.get('/github-oauth', async (req, res, next) => {});
+  // TODO: Comment
+  githubOAuthURL += '?';
+
+  // TODO: Comment
+  Object.keys(options).forEach((key, indx) => {
+    githubOAuthURL += key + '=' + encodeURIComponent(options[key]);
+    githubOAuthURL += '&';
+  });
+
+  // TODO: Comment
+  res.status(200).json({ url: githubOAuthURL });
+});
+
+router.get('/github-oauth', async (req, res, next) => {
+  let data = await githubMW(req);
+
+  // TODO: Comment
+  res.status(200).json({ name: data.name, email: data.email });
+});
 
 module.exports = router;
